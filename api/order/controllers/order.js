4ea2d2865786380f50420aa4dbec6a403cfe649b
IgorOleniuk <igor.oleniuk1@gmail.com>
2020-03-07 05:53:26 +0200

    commit

---
+++ api/order/controllers/order.js
@@ -0,0 +1,47 @@
+'use strict';
+const stripe = require('stripe')('pk_test_Ypq2OFU1GBlf6Vi8mqZSTlIc00bPqJsn5F');
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  create: async ctx => {
+    const {
+      address,
+      amount,
+      dishes,
+      postalCode,
+      token,
+      city,
+    } = ctx.request.body;
+
+    // Charge the customer
+    try {
+      await stripe.charges.create({
+        // Transform cents to dollars.
+        currency: 'usd',
+        description: `Order ${new Date()} by ${ctx.state.user.id}`,
+        source: token,
+      });
+
+      // Register the order in the database
+      try {
+        const order = await strapi.services.order.create({
+          user: ctx.state.user.id,
+          address,
+          amount,
+          dishes,
+          postalCode,
+          city,
+        });
+
+        return order;
+      } catch (e) {
+        // Silent
+      }
+    } catch (e) {
+
+    }
+  },
+};
